f87de4e63d3b7f4ef648f5e133aa952dac6b5ab3
wnitisak <jj_nitisak@hotmail.com>
2025-02-17 00:49:53 +0700

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
             <Head>
                 <Script src="https://kit.fontawesome.com/c405aac917.js" strategy='beforeInteractive' crossOrigin="anonymous" />
                 <meta name="viewport" content="width=device-width ,initial-scale=1,minimum-scale=1 ,maximum-scale=1,user-scalable=no" />
-                <link rel="icon" href="/logo.jpg" />
+                <link rel="icon" href="/logo.png" />
             </Head>
             <body>
                 <Main />
